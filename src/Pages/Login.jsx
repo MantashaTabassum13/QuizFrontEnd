e7ffdce0e416fad4b90b7b32b1c8fb08ebe9873b
Mantasha Tabassum <mantasha.tabassum16409@gmail.com>
2024-04-16 14:47:53 +0530

    changes added

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login1form from '../Components/Login1form'
+
+const Login = () => {
+  return (
+    <div>
+      <Login1form/>
+    </div>
+  )
+}
+
+export default Login
