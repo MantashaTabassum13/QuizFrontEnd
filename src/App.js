356c980995cf6566102bd1105e54687a213d9697
Mantasha Tabassum <mantasha.tabassum16409@gmail.com>
2024-04-18 12:15:10 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-// import Login from './Pages/Login'
-// import Quizz from './Components/Quizz'
+import Login from './Pages/Login'
+import Quizz from './Components/Quizz'
 import Home from './Pages/Home'
 import Navbar from './Pages/Common/Navbar'
+import Otp from './Components/Otp'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      {/* <Login/>
-      <Quizz/> */}
+      {/* <Login/> */}
+      {/* <Quizz/> */}
+      {/* <Otp/> */}
       <Home/>
      
     </div>
